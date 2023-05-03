b37d93122887db03d3603e1bfc62893a79bac1d7
노모 <129583916+n0Moment@users.noreply.github.com>
2023-05-03 18:05:02 +0900

    Fixed Build Error
    Sorry for Exception

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 base { archivesName.set(project.extra["archives_base_name"] as String) }
 version = project.extra["mod_version"] as String
-group = project.extra["maven_g  roup"] as String
+group = project.extra["maven_group"] as String
 
 val fabricLanguageKotlin =
     project.extra["fabric_language_kotlin_version"] as String + "+kotlin.${Dependency.Kotlin.Version}"
