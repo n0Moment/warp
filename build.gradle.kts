3ec4bd21251a3d3b7c22aa478ff9074838479b7e
n0Moment <no.moment.kr@gmail.com>
2023-06-16 19:11:28 +0900

    Add imgui - MIT License (noMoment)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import Build_gradle.IncludeMethod.INCLUDE
-import Build_gradle.IncludeMethod.NOT
-import Build_gradle.IncludeMethod.SHADOW
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -29,25 +26,11 @@ repositories {
         url = uri("https://jitpack.io")
         name = "JitPack"
     }
-}
-enum class IncludeMethod {
-    NOT, SHADOW, INCLUDE
+
+    maven("https://raw.githubusercontent.com/kotlin-graphics/mary/master")
 }
 
 dependencies {
-
-    val alpine_version: String by project
-
-
-    fun depend(includeMethod: IncludeMethod = NOT, notation: String, action: ExternalModuleDependency.() -> Unit = {}) {
-        implementation(dependencyNotation = notation, dependencyConfiguration = action)
-        when (includeMethod) {
-            SHADOW -> shadow(dependencyNotation = notation, dependencyConfiguration = action)
-            INCLUDE -> include(dependencyNotation = notation, dependencyConfiguration = action)
-            else -> {
-            }
-        }
-    }
 
     minecraft("com.mojang", "minecraft", Dependency.Minecraft.Version)
     mappings("net.fabricmc", "yarn", yarnBuild, null, "v2")
@@ -56,9 +39,9 @@ dependencies {
     modImplementation("net.fabricmc", "fabric-language-kotlin", fabricLanguageKotlin)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0-Beta")
     modImplementation("com.github.forceload:uilib:2a95cb42e3")
-
-
-    depend(SHADOW, "com.github.ZeroMemes:Alpine:$alpine_version")
+    implementation("kotlin.graphics:imgui-core:1.79+05")
+    implementation("kotlin.graphics:imgui-gl:1.79+05")
+    implementation("kotlin.graphics:imgui-glfw:1.79+05")
 
 }
 
